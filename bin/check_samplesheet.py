4d1c2fe568de9a4412cfc8d64ee1cb4a1d1f3e9d
Athanasios Baltzis <baltzis.athanasios@gmail.com>
2022-09-12 12:44:54 +0200

    Update ci tests and run black

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -10,7 +10,9 @@ import argparse
 
 
 def parse_args(args=None):
-    Description = "Reformat nf-core/proteinfold samplesheet file and check its contents."
+    Description = (
+        "Reformat nf-core/proteinfold samplesheet file and check its contents."
+    )
     Epilog = "Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
@@ -26,6 +28,7 @@ def make_dir(path):
         except OSError as exception:
             if exception.errno != errno.EEXIST:
                 raise exception
+
 
 def print_error(error, context="Line", context_str=""):
     error_str = "ERROR: Please check samplesheet -> {}".format(error)
@@ -56,7 +59,11 @@ def check_samplesheet(file_in, file_out):
         HEADER = ["sequence", "fasta"]
         header = [x.strip('"') for x in fin.readline().strip().split(",")]
         if header[: len(HEADER)] != HEADER:
-            print("ERROR: Please check samplesheet header -> {} != {}".format(",".join(header), ",".join(HEADER)))
+            print(
+                "ERROR: Please check samplesheet header -> {} != {}".format(
+                    ",".join(header), ",".join(HEADER)
+                )
+            )
             sys.exit(1)
 
         ## Check sample entries
@@ -73,7 +80,9 @@ def check_samplesheet(file_in, file_out):
             num_cols = len([x for x in lspl if x])
             if num_cols < MIN_COLS:
                 print_error(
-                    "Invalid number of populated columns (minimum = {})!".format(MIN_COLS),
+                    "Invalid number of populated columns (minimum = {})!".format(
+                        MIN_COLS
+                    ),
                     "Line",
                     line,
                 )
@@ -84,8 +93,8 @@ def check_samplesheet(file_in, file_out):
             if not sequence:
                 print_error("Sequence entry has not been specified!", "Line", line)
 
-            ## Check fasta file extension
-            #for fastq in [fastq_1, fastq_2]:
+                ## Check fasta file extension
+                # for fastq in [fastq_1, fastq_2]:
                 if fasta:
                     if fasta.find(" ") != -1:
                         print_error("fasta file contains spaces!", "Line", line)
@@ -120,11 +129,19 @@ def check_samplesheet(file_in, file_out):
             for sequence in sorted(sequence_mapping_dict.keys()):
 
                 ## Check that multiple runs of the same sample are of the same datatype
-                if not all(x[0] == sequence_mapping_dict[sequence][0][0] for x in sequence_mapping_dict[sequence]):
-                    print_error("Multiple runs of a sequence must be of the same datatype!", "Sequence: {}".format(sequence))
+                if not all(
+                    x[0] == sequence_mapping_dict[sequence][0][0]
+                    for x in sequence_mapping_dict[sequence]
+                ):
+                    print_error(
+                        "Multiple runs of a sequence must be of the same datatype!",
+                        "Sequence: {}".format(sequence),
+                    )
 
                 for idx, val in enumerate(sequence_mapping_dict[sequence]):
-                    fout.write(",".join(["{}_T{}".format(sequence, idx + 1)] + val) + "\n")
+                    fout.write(
+                        ",".join(["{}_T{}".format(sequence, idx + 1)] + val) + "\n"
+                    )
     else:
         print_error("No entries to process!", "Samplesheet: {}".format(file_in))
 
