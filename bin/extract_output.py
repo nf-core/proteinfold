0480d13567ae4ab6b4da7a056c2f9a5f5e7b30e7
Keiran Rowell <k.rowell@unsw.edu.au>
2025-03-19 17:25:43 +1100

    extract output now does all structs passed to it, and writes a warning num res differs

--- bin/extract_output.py
+++ bin/extract_output.py
@@ -5,16 +5,7 @@ import argparse
 from Bio import PDB
 import csv 
 
-%% to make into BioPython
-    awk '{print \$6"\\t"\$11}' ranked_0.pdb | uniq > ranked_0_plddt.tsv
-    for i in 1 2 3 4
-        do awk '{print \$6"\\t"\$11}' ranked_\$i.pdb | uniq | awk '{print \$2}' > ranked_"\$i"_plddt.tsv
-    done
-    paste ranked_0_plddt.tsv ranked_1_plddt.tsv ranked_2_plddt.tsv ranked_3_plddt.tsv ranked_4_plddt.tsv > plddt.tsv
-    echo -e Positions"\\t"rank_0"\\t"rank_1"\\t"rank_2"\\t"rank_3"\\t"rank_4 > header.tsv
-    cat header.tsv plddt.tsv > ../"${meta.id}"_plddt_mqc.tsv
-
-def extract_pLDDT(id, struct_files) 
+def extract_struct_pLDDT_to_tsv(id, struct_files): 
     """
     Uses the BioPython PDB package to extract residue pLDDT values from the b-factor column. Iterates over PDB objects rather than processes raw file
     Write out a tsv file for reading by MultiQC
@@ -23,18 +14,20 @@ def extract_pLDDT(id, struct_files)
     # Set up headers
     with open(id + '_plddt_mqc.tsv', 'w') as multiqc_tsv:
         writer = csv.writer(multiqc_tsv, delimiter='\t')
-        for ranks in range(struct_files):
-            rank_names.append(f"rank_{i}"}:
-        writer.writerow("Positions", rank_names)  
+        rank_names = []
+        for i in range(len(struct_files)):
+            rank_names.append(f"rank_{i}")
+        writer.writerow(["Positions"] + rank_names)  
 
     res_counts = [] 
+    pLDDT_cols = []    
 
     for struct_file in struct_files:
-
-        if f["fn"].endswith(".pdb"):
+        
+        if str(struct_file).endswith(".pdb"):
             parser = PDB.PDBParser(QUIET=True)
-            structure = parser.get_structure(id=samplename, file=struct_file)
-        elif f["fn"].endswith(".cif"):
+            structure = parser.get_structure(id=id, file=struct_file)
+        elif str(struct_file).endswith(".cif"):
             parser = PDB.MMCIFParser(QUIET=True)
             structure = parser.get_structure(structure_id=id, filename=struct_file)
         else:
@@ -42,6 +35,7 @@ def extract_pLDDT(id, struct_files)
     
     
         res_list = []
+        res_pLDDTs = [] # should probably use a numpy array
         pLDDT_tot = 0
 
         for model in structure:
@@ -53,12 +47,14 @@ def extract_pLDDT(id, struct_files)
                     num_atoms = len(atom_list)
                     res_pLDDT_tot = 0
                     for atom in residue:  # ESMFold and others have separate atom-wise values, so doing atom-wise to cover that and residue-wise
-                        atom_pLDDT = atom.get_bfactor()
+                        atom_pLDDT = round(float(atom.get_bfactor()),2)
                         res_pLDDT_tot += atom_pLDDT
                     
-                    res_pLDDT = res_pLDDT_tot / num_atoms
+                    res_pLDDT = round(float(res_pLDDT_tot / num_atoms),2)
+                    res_pLDDTs.append(res_pLDDT)
                     pLDDT_tot += res_pLDDT
 
+        
         num_res = len(res_list)
         res_counts.append(num_res)
         pLDDT_mean = pLDDT_tot / num_res
@@ -66,13 +62,25 @@ def extract_pLDDT(id, struct_files)
         if (pLDDT_mean < 1):  # Quirk of some programs is they report pLDDTs in decimals, but <1 pLDDTs are highly improbable, so let's just convert to percentage
             pLDDT_mean *= 100
 
+        pLDDT_cols.append(res_pLDDTs)
+
     # Check all structures have the same number of resiudes
     if all(x == res_counts[0] for x in res_counts) == False:
-        print("Not all structures have the same number of residues!"
+        print("Not all structures have the same number of residues!")
     else:
-        res_id_col = list(range(res_list))
-        
-        
+        res_id_col = list(range(len(res_list)))
+   
+    # Check the pLDDT cols are the same size before combining
+    if (len(set(len(col) for col in pLDDT_cols)) == 1) == False:
+        print("Not all pLDDT columns have the same number of values!")
+ 
+    pLDDT_rows = zip(res_id_col, *pLDDT_cols) #combine lists column-wise to make rows
+    
+    with open(id + '_plddt_mqc.tsv', 'a') as multiqc_tsv:
+        writer = csv.writer(multiqc_tsv, delimiter='\t')
+        for pLDDT_row in pLDDT_rows:
+            writer.writerow(pLDDT_row)
+    
 
 def read_pkl(id, pkl_files):
     for pkl_file in pkl_files:
@@ -94,11 +102,11 @@ def read_pkl(id, pkl_files):
 parser = argparse.ArgumentParser()
 parser.add_argument("--pkls", dest="pkls", required=True, nargs="+")
 parser.add_argument("--structs", dest="structs", required=True, nargs="+")
-parser.add_argument("--name", dest="name")
+parser.add_argument("--name", dest="name") # might need a --name $meta.id 
 parser.add_argument("--output_dir", dest="output_dir")
 parser.set_defaults(output_dir="")
 parser.set_defaults(name="")
 args = parser.parse_args()
 
 read_pkl(args.name, args.pkls)
-extract_plDDT_to_tsv(args.name, args.structs)
+extract_struct_pLDDT_to_tsv(args.name, args.structs)
