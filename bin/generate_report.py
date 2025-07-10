77fef3fce70f9d6248b4bf8307856921b7518a8a
JoseEspinosa <kadomu@gmail.com>
2025-07-10 22:52:02 +0200

    Fix tyop

--- bin/generate_report.py
+++ bin/generate_report.py
@@ -317,7 +317,7 @@ model_name = {
     "colabfold": "ColabFold",
     "rosettafold_all_atom": "Rosettafold_All_Atom",
     "helixfold3": "HelixFold3",
-    rosettafold2na: "RoseTTAFold2NA"
+    "rosettafold2na": "RoseTTAFold2NA"
 }
 
 parser = argparse.ArgumentParser()
